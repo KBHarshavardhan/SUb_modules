86da5aefd58ea30dde4e5140c3302282d4b27127
KBHarshavardhan <harsha260820@gmail.com>
2023-05-16 13:26:31 +0530

    SubTest

---
+++ SubTest/src/main/java/com/vitesco/SubTest/App.java
@@ -0,0 +1,13 @@
+package com.vitesco.SubTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
