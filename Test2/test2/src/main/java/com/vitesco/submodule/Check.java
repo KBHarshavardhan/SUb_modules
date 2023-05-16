98dafa77ecdb220e5feb40b395a47bea3e4b2539
KBHarshavardhan <harsha260820@gmail.com>
2023-05-16 12:03:17 +0530

    testing

---
+++ Test2/test2/src/main/java/com/vitesco/submodule/Check.java
@@ -0,0 +1,19 @@
+/*
+* COPYRIGHT VITESCO TECHNOLOGIES GROUP AG (EXCLUSIVE RIGHTS)
+*
+* Confidential
+*
+* The reproduction, transmission or use of this document or its contents is
+* not permitted without express written authority.
+* Offenders will be liable for damages. All rights, including rights created
+* by patent grant or registration of a utility model or design, are reserved.
+*/
+package com.vitesco.submodule;
+
+/**
+ *
+ */
+public class Check
+{
+
+}
